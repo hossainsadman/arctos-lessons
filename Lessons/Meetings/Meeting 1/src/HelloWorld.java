1fcbb9b26c9a8e3e634cce7ffe7e051902de34ee
Sadman Hossain <41028402+Sadman-Hossain@users.noreply.github.com>
2020-07-03 13:09:42 -0400

    clean up code

--- Lessons/Meetings/Meeting 1/src/HelloWorld.java
+++ Lessons/Meetings/Meeting 1/src/HelloWorld.java
@@ -10,41 +10,38 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         
-        /*
-        String a = "Something";        
-        System.out.println(a.length() + a.charAt(0));
-        System.out.println(a.substring(0, 2));
+        // String a = "Something";        
+        // System.out.println(a.length() + a.charAt(0));
+        // System.out.println(a.substring(0, 2));
         
-        int i = 10;
-        String p = "45";
-        // results in concatenation
-        System.out.println(i + p);
-        // results in error b/c cannot add integer to String
-        System.out.println(p + i);
-        System.out.println(i + Integer.parseInt(p));
+        // int i = 10;
+        // String p = "45";
+        // // results in concatenation
+        // System.out.println(i + p);
+        // // results in error b/c cannot add integer to String
+        // System.out.println(p + i);
+        // System.out.println(i + Integer.parseInt(p));
 
-        int num1 = 1;
-        String s = "45.0";
-        System.out.println((double) num1 + Double.parseDouble(s));
+        // int num1 = 1;
+        // String s = "45.0";
+        // System.out.println((double) num1 + Double.parseDouble(s));
 
-        System.out.println(String.valueOf(s));
-        System.out.println("" + s);
+        // System.out.println(String.valueOf(s));
+        // System.out.println("" + s);
 
-        Scanner input = new Scanner(System.in);
-        int r = input.nextInt();
-        System.out.println(r*2);
-        double inputDouble = input.nextDouble();
-        System.out.println(inputDouble);
-        */
-/*
-        BufferedReader = new BufferedReader(new InputStreamReader(in)
+        // Scanner input = new Scanner(System.in);
+        // int r = input.nextInt();
+        // System.out.println(r*2);
+        // double inputDouble = input.nextDouble();
+        // System.out.println(inputDouble);
 
-        try {
-            String input
-        }
-        catch(IOException v) {
-            v.printStackTrace();
-        }
-        */
+        // BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        // try {
+        //     String input;
+        // }
+        // catch(IOException v) {
+        //     v.printStackTrace();
+        // }
     }
 }
